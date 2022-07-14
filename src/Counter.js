3a449a1da8694df4812eeb3588e5043294a467b1
한정무 <99253941+ImportantHan@users.noreply.github.com>
2022-07-15 08:59:58 +0900

    기존 react 파일 합치기 기존 react 파일 합치기

---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+function Counter() {
+  const [number, dispatch] = useReducer(reducer, 0);
+
+  const onIncreast = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const onDecrease = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncreast}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  );
+}
+
+export default Counter;
